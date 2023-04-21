01d9adb553d34ca945322ec428faf4f12bc98031
evelina gracheva <yurova.evelina@outlook.com>
2023-04-21 16:36:45 +0400

    Add project files.

---
+++ Davaleba1/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string Username = "Evelina";
+        string Password = "Password";
+
+        int attempts = 0;
+        string username, password;
+        do
+        {
+            Console.Write("Enter your username: ");
+            username = Console.ReadLine();
+
+            Console.Write("Enter your password: ");
+            password = Console.ReadLine();
+
+            if (username == Username && password == Password)
+            {
+                Console.WriteLine("Login successful!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect username or password. Please try again.");
+                attempts++;
+            }
+        } 
+        while (attempts < 5);
+
+        Console.WriteLine("Error.Maximum number of login attempts exceeded.");
+    }
+}
