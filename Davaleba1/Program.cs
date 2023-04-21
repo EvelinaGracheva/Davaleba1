cc7c1771c47f1db0cca0f69ce26ae50b4e50e27f
evelina gracheva <yurova.evelina@outlook.com>
2023-04-21 22:14:40 +0400

    Davaleba3

--- Davaleba1/Program.cs
+++ Davaleba1/Program.cs
@@ -1,7 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System;
-
+﻿
 class Program
 {
     static void Main(string[] args)
--- Davaleba3/Program.cs
+++ Davaleba3/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int length = GetInput("length");
+        int width = GetInput("width");
+        int height = GetInput("height");
 
+        int area = CalculateArea(length, width);
+        Console.WriteLine($"Area: {area}");
 
+        int volume = CalculateVolume(length, width, height);
+        Console.WriteLine($"Volume: {volume}");
+    }
+
+    static int GetInput(string dimension)
+    {
+        Console.Write($"Enter {dimension}: ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    static int CalculateArea(int length, int width)
+    {
+        return length * width;
+    }
+
+    static int CalculateVolume(int length, int width, int height)
+    {
+        return length * width * height;
+    }
+}
