7bae016bf183aecc7febb47c654efa9d03687778
evelina gracheva <yurova.evelina@outlook.com>
2023-04-21 18:06:16 +0400

    davaleba2

---
+++ Davaleba2/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a decimal number: ");
+        int decimalNumber = int.Parse(Console.ReadLine());
+
+        string binaryNumber = Convert.ToString(decimalNumber, 2);
+
+        Console.WriteLine("Binary representation: " + binaryNumber);
+    }
+}
